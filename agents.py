8ebbe3e0574c39fb19cff43874c569e2a35c4ce6
John-Progr <john314325@gmail.com>
2025-10-13 18:50:26 +0300

    Made the code lab testing friendly

--- agents.py
+++ agents.py
@@ -2,10 +2,11 @@ import numpy as np
 
 
 class EpsilonGreedy:
-    def __init__(self, n_arms, epsilon, update_rule):
+    def __init__(self, n_arms, epsilon, update_rule, alpha):
         self.n_arms = n_arms
         self.epsilon = epsilon
         self.update_rule = update_rule
+        self.alpha = alpha
         self.counts = np.zeros(n_arms)   # Number of times each arm is pulled, basically how often each channel is used 
         self.values = np.zeros(n_arms)   # Estimated values of each arm, basically estimated throughput per channel
 
@@ -22,14 +23,18 @@ class EpsilonGreedy:
         n = self.counts[chosen_arm]
         value = self.values[chosen_arm]
 
-        if update_rule == "incremental":
-        
-        elif update_rule == "exponential_smoothing":
+        if self.update_rule == "incremental":
 
-        # This is the incremental update rule of shutton and burton book
-        # they prove this and the general form is 
-        # NewEstimate = OldEstimate + StepSize[Target-OldEstimate]
-        # step size denoted by at(a), a = 1/k (in a more informal way)
-        self.values[chosen_arm] = value + (reward - value) / n
+            # This is the incremental update rule of shutton and burton book
+            # they prove this and the general form is 
+            # NewEstimate = OldEstimate + StepSize[Target-OldEstimate]
+            # step size denoted by at(a), a = 1/k (in a more informal way)
+            self.values[chosen_arm] = value + (reward - value) / n
+
+        elif self.update_rule == "exponential_smoothing":
+            self.values[chosen_arm] = value + self.alpha *(reward-value)
+
+        else:
+            raise ValueError("update_rule must be 'incremental' or 'exponential_smoothing'")
 
 
